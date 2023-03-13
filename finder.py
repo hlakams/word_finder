b4ed263e25eac59f40ace8b9b1dcb0aa259edbee
Harsha Lakamsani <hlakamsani@gmail.com>
2023-03-13 00:28:02 -0700

    finder method now verifies independence

--- finder.py
+++ finder.py
@@ -1,21 +1,65 @@
 # find all instances of given strings within input
-def finder(input_string: str, to_find: list[str]) -> list[int]:
+def finder(input_string: str, to_find: list[str], to_verify: list[bool]) -> list[int]:
+    # all found positions, grouped by word
     instances = []
     input_string = input_string.lower()
     
-    for current_string in to_find:
+    # iterate over all words to find
+    for idx, current_string in enumerate(to_find):
+        # account for all foung instances for current word 
         current_string = current_string.lower()
         current_instances = []
         current_idx = 0
 
+        # loop over all indices in input string
         while current_idx < len(input_string):
-            if current_idx + len(current_string) <= len(input_string):
+            # current word to find is valid
+            if current_idx + len(current_string) - 1 < len(input_string):
+                # word is found
                 if input_string[current_idx:current_idx+len(current_string)] == current_string:
-                    current_instances.append(current_idx)
+                    # if word can be substring, add found index 
+                    if to_verify[idx] == False:
+                        current_instances.append(current_idx)
+                    # if word cannot be substring, verify independence + add found index
+                    else:
+                        if verify_word(input_string, current_string, current_idx):
+                            current_instances.append(current_idx)
+                
+                # go to next position
                 current_idx += 1
+            # invalid word
             else:
                 break
-        
+
+        # add found indices for current word to ledger
         instances.append(current_instances)
 
+    # done!
     return instances
+
+# check if word is separated
+def verify_word(input_string: str, current_string: str, current_idx: int) -> bool:
+    # boundary conditions: word is at start, middle, or end (all cases, by itself)
+    # check start idx
+    if current_idx == 0:
+        # start and end match
+        if len(input_string) == len(current_string):
+            return True
+        # start matches, end doesn't:
+        elif input_string[current_idx+len(current_string)] == ' ':
+            return True
+    # start idx is not 0 (check before and after word)
+    else:
+        # string is set apart at end, and portion after is valid
+        if input_string[current_idx - 1] == ' ':
+            # portion of string afterwards is followed by space
+            if current_idx + len(current_string) < len(input_string):
+                # space found
+                if input_string[current_idx+len(current_string)] == ' ':
+                    return True
+            # end of input string
+            else:
+                return True
+    
+    # invalid condition, some boudary condition failed
+    return False
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from finder import finder
 
 # strings to find
 to_find = ['mouse', 'mice']
+to_verify = [True, False]
 
 # initial info prompt
 print('This program finds the start positions of the words provided in `main.py` as seen in your input string.')
@@ -11,11 +12,16 @@ print('This program finds the start positions of the words provided in `main.py`
 input_string = input('Enter your input string: ')
 
 # get result
-result = finder(input_string, to_find)
+result = finder(input_string, to_find, to_verify)
 
 # find instances + print to console
-print('\nStarting locations of given input strings:')
+print('\nStarting locations of given strings to find:')
 for idx, result in enumerate(result):
-    print(f'Results for {to_find[idx]}:')
-    for value in result:
-        print(value)
+    # no results found
+    if len(result) == 0:
+        print(f'No results for {to_find[idx]}!')
+    # results found
+    else:
+        print(f'Results for {to_find[idx]}:')
+        for value in result:
+            print(value)
