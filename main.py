1f14412f115d0d345cae3d8b11187f7a60ee0a1f
Harsha Lakamsani <hlakamsani@gmail.com>
2023-03-13 00:59:48 -0700

    implemented custom input interface

--- main.py
+++ main.py
@@ -7,9 +7,34 @@ to_verify = [True, False]
 
 # initial info prompt
 print('This program finds the start positions of the words provided in `main.py` as seen in your input string.')
+# check if user wants custom strings to find
+custom_find = input('Do you want to use the default strings to find [Y/n]? ')
+find_verify = ['yes', 'y', '']
+
+# add strings to find
+if custom_find.lower().strip() not in find_verify:
+    # remove old strings
+    to_find.clear()
+    to_verify.clear()
+    custom_find = ''
+    # keep adding strings to find
+    while custom_find.lower().strip() in find_verify:
+        # new string
+        new_find = input('\nEnter a custom string to find: ')
+        to_find.append(new_find)
+        
+        # singular find condition
+        new_verify = input('Do you want to find singular instances only (substrings by default) [Y/n]? ')
+        if new_verify in find_verify:
+            to_verify.append(True)
+        else:
+            to_verify.append(False)
+
+        # prompt user for more strings
+        custom_find = input('Do you want add more custom strings to find [Y/n]? ')
 
 # input string
-input_string = input('Enter your input string: ')
+input_string = input('\nEnter your input string: ')
 
 # get result
 result = finder(input_string, to_find, to_verify)
