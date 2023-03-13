1afcf8c7a8e66c3d21694ef4eb6456a801fff4f4
Harsha Lakamsani <hlakamsani@gmail.com>
2023-03-12 19:29:07 -0700

    index finding without conditionals

--- finder.py
+++ finder.py
@@ -1,3 +1,18 @@
 # find all instances of given strings within input
 def finder(input_string: str, to_find: list[str]) -> list[int]:
-    return [0,0]
+    instances = []
+    input_string = input_string.lower()
+    
+    for current_string in to_find:
+        current_string = current_string.lower()
+        current_idx = 0
+
+        while current_idx < len(input_string):
+            if current_idx + len(current_string) < len(input_string):
+                if input_string[current_idx:current_idx+len(current_string)] == current_string:
+                    instances.append(current_idx)
+                current_idx += 1
+            else:
+                break
+
+    return instances
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from finder import finder
 # strings to find
 to_find = ['mouse', 'mice']
 
+# initial info prompt
+print('This program finds the start positions of the words provided in `main.py` as seen in your input string.')
+
 # input string
 input_string = input('Enter your input string: ')
 
