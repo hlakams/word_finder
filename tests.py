8a77b841d7c013ae6d6b8c6055de994d2939f639
Harsha Lakamsani <hlakamsani@gmail.com>
2023-04-10 19:36:45 -0700

    remove fluff from tests

--- tests.py
+++ tests.py
@@ -9,12 +9,12 @@ to_verify = [True, False]
 # this test is for appearances of the found word as a proper substring, rather than a constituent substring
 # (PASS)
 def test_finder_1() -> None:
-    output = finder.finder('mousecat mice catmice mouse mousemouse', ['mouse'], [True])
+    output = finder.finder('mousecat mice catmice mouse mousemouse', to_find[0], to_find[1])
     assert output == [[22]]
 
 
 # this test is for appearances of the found word as a substring in any form
 # (FAIL)
 def test_finder_2() -> None:
-    output = finder.finder('mousecat mice catmice mouse mousemouse', ['mice'], [False])
+    output = finder.finder('mousecat mice catmice mouse mousemouse', to_find[1], to_verify[1])
     assert output == [[9, 17]]
