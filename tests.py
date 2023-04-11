6c2188852b5ac3ce616ef05cc3e4fc5cac9012bb
Harsha Lakamsani <hlakamsani@gmail.com>
2023-04-10 19:39:39 -0700

    comments explaining tests setup

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@
 import finder
 
 # define base cases
+# 'mouse' is a proper substring, mice is a constituent substring
 to_find = ['mouse', 'mice']
 to_verify = [True, False]
 
@@ -9,6 +10,7 @@ to_verify = [True, False]
 # this test is for appearances of the found word as a proper substring, rather than a constituent substring
 # (PASS)
 def test_finder_1() -> None:
+    # we expect to find 'mouse' as a proper substring that starts at index 22
     output = finder.finder('mousecat mice catmice mouse mousemouse', to_find[0], to_find[1])
     assert output == [[22]]
 
@@ -16,5 +18,6 @@ def test_finder_1() -> None:
 # this test is for appearances of the found word as a substring in any form
 # (FAIL)
 def test_finder_2() -> None:
+    # we expect to find 'mice' as a constituent substring, two of which start at indices 9 and 17
     output = finder.finder('mousecat mice catmice mouse mousemouse', to_find[1], to_verify[1])
     assert output == [[9, 17]]
